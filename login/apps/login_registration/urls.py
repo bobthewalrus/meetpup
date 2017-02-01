8201dc1e129be980281e5a50fb71e48b6fbfa166
James Chantler <chantler.james@gmail.com>
2017-02-01 10:52:37 -0800

    Merge conflict resolution

--- login/apps/login_registration/urls.py
+++ login/apps/login_registration/urls.py
@@ -1,23 +1,14 @@
 from django.conf.urls import url
-from views import index, loginvalidate, registervalidate, success, logout, zipupdate
-<<<<<<< HEAD
-from views import index, loginvalidate, registervalidate, success, logout, register, community, adoption
-=======
-from views import index, loginvalidate, registervalidate, success, logout
+from views import index, loginvalidate, registervalidate, success, logout, zipupdate, register, community, adoption
 from . import views
->>>>>>> sam
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^loginvalidate$', loginvalidate, name='loginvalidate'),
     url(r'^registervalidate$', registervalidate, name='registervalidate'),
     url(r'^success$', success, name='success'),
-    url(r'^logout$', logout, name='logout'),
-<<<<<<< HEAD
     url(r'^register$', register, name='register'),
     url(r'^community$', community, name='community'),
-    url(r'^adoption$', adoption, name='adoption')
-=======
->>>>>>> sam
+    url(r'^adoption$', adoption, name='adoption'),
     url(r'^logout$', logout, name='logout'),
     url(r'^zipupdate$', zipupdate, name='zipupdate')
 ]
