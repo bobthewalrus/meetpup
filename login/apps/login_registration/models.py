e9572aa3f2091cb3e58af4f485bd01bc49a966bf
Jeff <jhwe92@gmail.com>
2017-02-01 17:15:44 -0800

    added bio attribute to Pet class

--- login/apps/login_registration/models.py
+++ login/apps/login_registration/models.py
@@ -97,6 +97,7 @@ class User(models.Model):
 class Pet(models.Model):
     name = models.CharField(max_length=225)
     birthday = models.DateField()
+    biography = models.TextField()
     breed = models.CharField(max_length=150)
     zipcode = models.IntegerField()
     user = models.ForeignKey(User)
